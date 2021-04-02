c843ad7445317283040c9b90704135da3a60dfea
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-04-01 20:32:36 -0500

    changes

--- shopping_proj/settings.py
+++ shopping_proj/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os 
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +25,7 @@ SECRET_KEY = '1!^@ovx7xc8%!-4jjj_nl4b^(otis!$qv2)jv(g9!vavfjnru^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://python-projectmax.herokuapp.com/"]
 
 
 # Application definition
